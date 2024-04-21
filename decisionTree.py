dc50e76e00c98b1d694e16282a99ac27c794ac7f
dinixz <148535002+dinixz@users.noreply.github.com>
2024-04-21 12:23:31 +0100

    conditional entropy e decision tree com sklearn

--- decisionTree.py
+++ decisionTree.py
@@ -1,13 +1,40 @@
 import pandas as pd, numpy as np, sklearn, matplotlib as plt
 from restaurant import df
 
-def entropy(df:pd.DataFrame, atribute:str):
-    length = df.shape[0]
-    atribute_values = df[atribute].value_counts().to_dict()
-    h = 0
-    for value, times in atribute_values.items():
-        prob = times / length
-        h -= prob * np.log2(prob)
-    return h 
-    
-print(entropy(df, 'Fri'))
+class TreeNode:
+    def __init__(self, condition=None, value=None, children=[]) -> None:
+        self.value = value
+        self.children = children
+        self.condition = condition
+
+def entropy(df:pd.DataFrame, attribute:str) -> float:
+    values = df[attribute].unique()
+    entropy = 0
+
+    for value in values:
+        subset_df = df[df[attribute] == value]
+        prob = len(subset_df) / len(df)
+        entropy -= prob * np.log2(prob)
+    return entropy
+
+def conditional_entropy(df:pd.DataFrame, attribute:str, target_attribute:str) -> float: #H( attribute | target_attribute )
+    target_classes = df[target_attribute].unique()
+    entropy_attribute = 0
+    total_examples = len(df)
+
+    for cls in target_classes:
+        subset_df = df[df[target_attribute] == cls]
+        entropy_subset = entropy(subset_df, attribute)
+        prob_cls = len(subset_df) / total_examples #probabilidade desta classe acontecer P(cls)
+        entropy_attribute += prob_cls * entropy_subset
+    return entropy_attribute
+
+def information_gain(df:pd.DataFrame, attribute:str) -> float:
+    return entropy(df, 'Class') + conditional_entropy(df, 'Class', attribute)
+
+attributes = df.columns.tolist()
+attributes.remove('Class')
+print(attributes)
+
+for attribute in attributes:
+    print(attribute + ': ' + str(information_gain(df, attribute)))
--- restaurant.py
+++ restaurant.py
@@ -18,20 +18,7 @@ for column in df.columns:
         label_encoder = sklearn.preprocessing.LabelEncoder() #inicia encoder
         df[column + '_num'] = label_encoder.fit_transform(df[column]) #cria nova coluna com os valores codificados
 
-        #para guardar num dict o numero associado ao valor real
-        to_num = {}
-        for value in set(df[column].values.tolist()):
-            num = df.loc[df[column] == value, column + '_num'].to_list()[0]
-            to_num[num] = value
-
-        one_hot_encoder = sklearn.preprocessing.OneHotEncoder() #inicia encoder
-        num_reshaped = df[column + '_num'].values.reshape(-1, 1) #da reshape para matriz
-        one_hot_encoder.fit(num_reshaped) #fit ao encoder
-        ohe = one_hot_encoder.transform(num_reshaped)
-        ohe_array = ohe.toarray()
-        ohe_df = pd.DataFrame(ohe_array, columns=[f'{to_num.get(i)}' for i in range(ohe_array.shape[1])]) #novo dataframe
-        df = pd.concat([df, ohe_df], axis=1) #junta o novo df
-
-        df = df.drop([column, column + '_num'], axis=1)
-    
+        df = df.drop([column], axis=1)
+        
 print(df)
+    
