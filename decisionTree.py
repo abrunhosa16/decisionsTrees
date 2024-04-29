3fe0239e05f48c9bdc5b3d1e472740752264c031
dinixz <148535002+dinixz@users.noreply.github.com>
2024-04-29 12:21:22 +0100

    max_gini

--- decisionTree.py
+++ decisionTree.py
@@ -39,21 +39,24 @@ class DecisionTreeClassifier:
     def split_features_target(self, dataset: pd.DataFrame) -> list: #return lista com nome das features e o target
         return dataset.columns[:-1], dataset.columns[-1]
         
-    def build_tree(self, dataset: pd.DataFrame, cur_depth: int = 0) -> Node:
-        features, target = self.split_features_target(dataset)
-        x = dataset[features] 
+    def build_tree(self, dataset: pd.DataFrame, remaining_features: list, cur_depth: int= 1) -> Node:
+        _, target = self.split_features_target(dataset)
+        x = dataset[remaining_features] 
         y = dataset[target]
         
         num_samples, _ = x.shape
         
-        if num_samples >= self.min_samples_split and cur_depth <= self.max_depth:
-            best_split = self.get_best_split(dataset) #dict com feature, info_gain, k: k_dataset
+        if num_samples >= self.min_samples_split and cur_depth <= self.max_depth and len(remaining_features) > 0:
+            best_split = self.get_best_split(dataset, remaining_features) # dict com feature, info_gain, k: k_dataset
             
-            if best_split['info_gain'] > 0:
-                
+            if best_split['info_gain'] < 1:
+
                     children = []    
+                    remaining_features.remove( best_split['feature'] )
                     for value, child_dataset in best_split['datasets'].items():
-                        subtree = self.build_tree(dataset= child_dataset, cur_depth= cur_depth+1)
+                        
+
+                        subtree = self.build_tree(dataset= child_dataset, remaining_features= remaining_features, cur_depth= cur_depth+1)
                         subtree.set_condition(value) #o valor é a condição referente à feature do pai
                         children.append(subtree)
                         
@@ -62,25 +65,22 @@ class DecisionTreeClassifier:
         leaf_value = self.calculate_leaf_value(y)
         return Node(leaf_value= leaf_value)     
     
-    def b(self, q: float) -> float:
-        return -(q*np.log2(q) + (1-q)*np.log2(1-q)) if q not in {0, 1} else 0
-     
-    def gini_class(self, dataset: pd.DataFrame, feature): # Calcula o indice gini de cada valor em uma feature e retorna um dicionario com cada valor e seu respectivo resultado
+    #Cálculo de gini
+    def gini_class(self, dataset: pd.DataFrame, feature: str) -> dict: # Calcula o indice gini de cada valor em uma feature e retorna um dicionario com cada valor e seu respectivo resultado
         _, target = self.split_features_target(dataset)
         values_target = dataset[target].unique()
         values_feature = dataset[feature].unique()
         gini_dic = {}
         tamanho_feature = len(dataset[feature])
-        for val in values_feature:
+        for value in values_feature:
             soma = 1
             for j in values_target:
-                a = len(dataset[(dataset[feature]== val) & (dataset[target] == j)])
+                a = len(dataset[(dataset[feature]== value) & (dataset[target] == j)])
                 soma -= (a/tamanho_feature)**2
-            gini_dic[val] = soma
+            gini_dic[value] = soma
         return gini_dic
 
-
-    def gini_split(self, dataset, feature): # calcula o indice gini total da feature 
+    def gini_split(self, dataset: pd.DataFrame, feature: str) -> float: # calcula o indice gini total da feature 
         dic = self.gini_class(dataset, feature)
         values = dic.keys()
         soma = 0
@@ -89,6 +89,15 @@ class DecisionTreeClassifier:
             soma += (len(dataset[dataset[feature] == i])/tamanho) * dic[i]
         return soma
 
+    def max_gini(self, dataset: pd.DataFrame, features: list) -> tuple: # (feature, max_gini)
+        ginis = [self.gini_split(dataset, feature) for feature in features]
+        final_gini = min(ginis)
+        return (features[ ginis.index(final_gini) ], final_gini)
+    
+    #Cálculo ganho de informação
+    def b(self, q: float) -> float:
+        return -(q*np.log2(q) + (1-q)*np.log2(1-q)) if q not in {0, 1} else 0
+     
     def remainder(self, dataset: pd.DataFrame, feature: str) -> float:
         _, target = self.split_features_target(dataset)
         class_counts = dataset[target].value_counts()
@@ -114,10 +123,17 @@ class DecisionTreeClassifier:
         features, _ = self.split_features_target(dataset)
         info_gains = [self.information_gain(dataset, feature) for feature in features]
         info_gain = max(info_gains)
-        return (features[info_gains.index(info_gain)], info_gain)
- 
-    def get_best_split(self, dataset: pd.DataFrame) -> dict: #dict com feature, info_gain e k: k_dataset
-        feature, info_gain = self.max_info_gain(dataset)
+        return (features[ info_gains.index(info_gain) ], info_gain)
+
+    #obtem o melhor split de dados
+    def get_best_split(self, dataset: pd.DataFrame, features: list) -> dict: #dict com feature, info_gain e k: k_dataset
+        # _, target = self.split_features_target(dataset)
+        
+        #verificar se a target é binaria ou nao
+        # if len(dataset[target].unique()) <= 2:
+        #     feature, gain = self.max_info_gain(dataset)
+        # else:
+        feature, gain = self.max_gini(dataset, features)
         feature_values = dataset[feature].unique()
         child_datasets = {}
         for value in feature_values: #separa em datasets filhos para cada valor da feature
@@ -126,17 +142,20 @@ class DecisionTreeClassifier:
             
         best_split = {}    
         best_split['feature'] = feature
-        best_split['info_gain'] = info_gain
+        best_split['info_gain'] = gain
         best_split['datasets'] = child_datasets
         return best_split
-        
+    
+    #obtem o valor de folha mais comum
     def calculate_leaf_value(self, y: pd.DataFrame) -> int:
         y = list(y)
         return max(y, key= y.count)
     
-    def fit(self, x: pd.DataFrame, y: pd.DataFrame) -> None: #função para construir a DT
+    #construçao da DT
+    def fit(self, x: pd.DataFrame, y: pd.DataFrame) -> None: 
         dataset = pd.concat((x,y), axis=1)
-        self.root = self.build_tree(dataset= dataset)
+        features = x.columns.to_list()
+        self.root = self.build_tree(dataset= dataset, remaining_features= features)
         
     def predict(self, X: pd.DataFrame) -> list:
         predictions = [self.make_prediction(row, self.root) for _, row in X.iterrows()]
@@ -149,6 +168,7 @@ class DecisionTreeClassifier:
         for child in tree.children:
             if feature_value == child.condition:
                 return self.make_prediction(x, child)
+        print('ERROR')
         
     def print_tree(self, node: Node, indent= ""):
         if node is None:
@@ -179,15 +199,16 @@ def split_representative(df: pd.DataFrame, perc: float) -> float:
 
         return train, test
         
-# train_df, test_df = split_representative(df, 'Class', 0.2)
-# x_train = train_df.iloc[:, :-1]
-# y_train = train_df.iloc[:, -1]
+train_df, test_df = split_representative(restaurant_df, 0.2)
+x_train = train_df.iloc[:, :-1]
+y_train = train_df.iloc[:, -1]
 
-# x_test = test_df.iloc[:, :-1]
-# y_test = test_df.iloc[:, -1]
+x_test = test_df.iloc[:, :-1]
+y_test = test_df.iloc[:, -1]
 
-# classifier = DecisionTreeClassifier(min_samples_split= 7, max_depth= 30)
-# classifier.fit(x_train, y_train)
+classifier = DecisionTreeClassifier(min_samples_split= 3, max_depth= 3)
+classifier.fit(x_train, y_train)
+classifier.print_tree(classifier.root)
 
 # print(classifier.predict(x_test))
 # print(y_test)
@@ -221,7 +242,7 @@ def precision(dataframe):
                 positivos += 1
             total += 1
     return positivos/total
-print(precision(weather_df))
+print(precision(restaurant_df))
 
 
 def entropy(a) -> float:
@@ -248,4 +269,4 @@ def point_split(df, attribute, extremos: tuple): # função para avaliar onde fa
             acc.append(ent)
     minimo = min(acc)
     return (minimo,acc.index(minimo))
-print(point_split(weather_df, 'Temp', (50, 100)))
+# print(point_split(weather_df, 'Temp', (50, 100)))
